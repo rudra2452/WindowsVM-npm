566c4ee395a4a669933f0f9fc5b590d052a8361e
rudra2452 <prem1905@hotmail.com>
2025-12-26 14:23:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Node.js app
+
+A minimal Express server for testing on an Azure Windows VM.
+
+## Run locally
+```bash
+npm install
+node index.js
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Azure VM!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
